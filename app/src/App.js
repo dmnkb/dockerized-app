b8c3b2fd019df37613afe4c09abf12be19f36afd
Dominik <dominik@borchert.me>
2022-01-07 16:44:59 +0100

    Development setup, mysql db

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useCallback, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hallo Welt (CI Test)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const TodoListCard = () => {
+	const [items, setItems] = useState(null);
+
+	useEffect( () => {
+		fetch('http://localhost:3000/api/items')
+		.then(r => r.json())
+		.then(setItems);
+	}, []);
+
+	const onNewItem = useCallback( newItem => { 
+		setItems([...items, newItem]);
+	}, [items]);
+	
+  	const onItemUpdate = useCallback( item => {
+		const index = items.findIndex(i => i.id === item.id);
+		setItems([...items.slice(0, index), item, ...items.slice(index + 1)]);
+	}, [items]);
+
+  	const onItemRemoval = useCallback( item => {
+		let index = items.findIndex(i => i.id === item.id);
+		setItems([...items.slice(0, index), ...items.slice(index + 1)]);
+	}, [items]);
+	
+  	return !items ? "Loading..." :
+		<>
+			<AddItemForm onNewItem={onNewItem} />
+			{items.length === 0 && (
+				<p className="text-center">You have no todo items yet! Add one above!</p>
+			)}
+			{items.map(item => (
+				<ItemDisplay
+					item={item}
+					key={item.id}
+					onItemUpdate={onItemUpdate}
+					onItemRemoval={onItemRemoval}
+				/>
+			))}
+		</>
+}
+
+const AddItemForm = ({ onNewItem }) => {
+
+  	const [newItem, setNewItem] = useState('');
+  	const [submitting, setSubmitting] = useState(false);
+
+  	const submitNewItem = e => {
+    	e.preventDefault();
+		setSubmitting(true);
+		fetch('http://localhost/api/items', {
+			method: 'POST',
+			body: JSON.stringify({ name: newItem }),
+			headers: { 'Content-Type': 'application/json' },
+      	})
+		.then(r => r.json())
+		.then(item => {
+			onNewItem(item);
+			setSubmitting(false);
+			setNewItem('');
+		});
+  	};
+
+  	return (
+		<form onSubmit={submitNewItem}>
+			<input
+				value={newItem}
+				onChange={e => setNewItem(e.target.value)}
+				type="text"
+				placeholder="New Item"
+				aria-describedby="basic-addon1"
+        	/>
+			<button
+				type="submit"
+				variant="success"
+				disabled={!newItem.length}
+				className={submitting ? 'disabled' : ''}
+			>
+				{submitting ? 'Adding...' : 'Add'}
+			</button>
+      	</form>
+  	);
+}
+
+const ItemDisplay = ({ item, onItemUpdate, onItemRemoval }) => {
+	const toggleCompletion = () => {
+		fetch(`http://localhost/api/items/${item.id}`, {
+			method: 'PUT',
+			body: JSON.stringify({
+				name: item.name,
+				completed: !item.completed,
+			}),
+			headers: { 'Content-Type': 'application/json' },
+		})
+		.then(r => r.json())
+		.then(onItemUpdate);
+  	};
+
+	const removeItem = () => {
+		fetch(`http://localhost/api/items/${item.id}`, { method: 'DELETE' }).then(() =>
+			onItemRemoval(item),
+      	);
+  	};
+
+	return (
+		<>
+			<button onClick={toggleCompletion}>
+				{item.completed ? '✅' : '❌'}
+			</button>
+			{item.name}
+			<button onClick={removeItem} aria-label="Remove Item">
+				Löschen
+			</button>
+      	</>
+  	);
+}
+
+const App = () => {
+	return <TodoListCard />
 }
 
 export default App;
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,38 @@
 const express = require("express")
-
+const cors = require('cors');
 const app = express()
+const db = require('./mysql');
+const getItems = require('./routes/getItems');
+const addItem = require('./routes/addItem');
+const updateItem = require('./routes/updateItem');
+const deleteItem = require('./routes/deleteItem');
+
+app.use(express.json());
+app.use(cors());
+app.options('*', cors())
 
 app.get('/api', (req, res) => {
-  res.send('<h1>Hallo Welt Backend</h1>')
+  res.send('<h1>Backend</h1>')
+});
+app.get('/api/items', getItems);
+app.post('/api/items', addItem);
+app.put('/api/items/:id', updateItem);
+app.delete('/api/items/:id', deleteItem);
+
+
+db.init().then(() => {
+	app.listen(3000, () => console.log('Listening on port 3000'));
+}).catch((err) => {
+	console.error(err);
+	process.exit(1);
 });
 
-app.listen(3000, () => {
-  console.log('App listening on port 3000!')
-})
+const gracefulShutdown = () => {
+  db.teardown()
+      .catch(() => {})
+      .then(() => process.exit());
+};
+
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon
